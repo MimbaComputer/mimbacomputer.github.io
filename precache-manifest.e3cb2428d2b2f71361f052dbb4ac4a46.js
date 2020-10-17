bd3a994dcc11ca3083ac2817928d84c16a0cc18f
Mimba Ngouana Fofou <baounabaouna@gmail.com>
2020-10-17 18:37:06 +0100

    adding 17-10-2020

---
+++ precache-manifest.e3cb2428d2b2f71361f052dbb4ac4a46.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c25e8ef14c007862944",
+    "url": "/css/app.6f626c16.css"
+  },
+  {
+    "revision": "b89d7f809803958fb7ad",
+    "url": "/css/chunk-vendors.e33783dd.css"
+  },
+  {
+    "revision": "221bc05deb4c42a14ee8",
+    "url": "/css/connexion-1.526193d9.css"
+  },
+  {
+    "revision": "d32895f072a1d1e80079",
+    "url": "/css/connexion-2.2825d574.css"
+  },
+  {
+    "revision": "ec3cf7cb79f900b319a6",
+    "url": "/css/garde-detail.e7c3fa7e.css"
+  },
+  {
+    "revision": "d75a35386f12912eee02",
+    "url": "/css/localisation-detail.7a7a96c1.css"
+  },
+  {
+    "revision": "9c45190c13ed3ca5775d",
+    "url": "/css/localisation-edit.09041200.css"
+  },
+  {
+    "revision": "be2315378c6506737ee3",
+    "url": "/css/localisation-list.d5606144.css"
+  },
+  {
+    "revision": "726dea314f0ad384b330",
+    "url": "/css/pharmacie-detail.0bb3811e.css"
+  },
+  {
+    "revision": "8dd2861b18bdd05bd71a7e4a2e4b57bb",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "8dd2861b18bdd05bd71a7e4a2e4b57bb",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "8dd2861b18bdd05bd71a7e4a2e4b57bb",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "8dd2861b18bdd05bd71a7e4a2e4b57bb",
+    "url": "/icon.png"
+  },
+  {
+    "revision": "2dccace6b57c5f1ab9ea949db2395c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "662ffc7cfe2acb5c00a3",
+    "url": "/js/about.8267fb0d.js"
+  },
+  {
+    "revision": "6c25e8ef14c007862944",
+    "url": "/js/app.f98f5829.js"
+  },
+  {
+    "revision": "b89d7f809803958fb7ad",
+    "url": "/js/chunk-vendors.e1dc642b.js"
+  },
+  {
+    "revision": "221bc05deb4c42a14ee8",
+    "url": "/js/connexion-1.9383452b.js"
+  },
+  {
+    "revision": "d32895f072a1d1e80079",
+    "url": "/js/connexion-2.316e1ecd.js"
+  },
+  {
+    "revision": "6e4525e1ca7cb3e600ca",
+    "url": "/js/connexion-3.bcd12a71.js"
+  },
+  {
+    "revision": "ec3cf7cb79f900b319a6",
+    "url": "/js/garde-detail.4df073c0.js"
+  },
+  {
+    "revision": "d75a35386f12912eee02",
+    "url": "/js/localisation-detail.6825e523.js"
+  },
+  {
+    "revision": "9c45190c13ed3ca5775d",
+    "url": "/js/localisation-edit.a94e8a53.js"
+  },
+  {
+    "revision": "be2315378c6506737ee3",
+    "url": "/js/localisation-list.839d1de3.js"
+  },
+  {
+    "revision": "726dea314f0ad384b330",
+    "url": "/js/pharmacie-detail.5156d63c.js"
+  },
+  {
+    "revision": "ac4699b3c0d572601ff860d3a1b9d8be",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
